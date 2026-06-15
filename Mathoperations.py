09a0a84475c440b1e08319ff1cdcba1affe69f3a
Krushnali Kaloge <krushnalikaloge@gmail.com>
2026-06-15 20:28:46 +0530

    Add files via upload

---
+++ Mathoperations.py
@@ -0,0 +1,12 @@
+x = int(input("Enter the first number :"))
+y = int(input("Enter the second number :"))
+
+add  = x + y
+sub  = x - y
+mult = x * y
+div  = x / y
+
+print("Addition is :",add)
+print("Subtration is :",sub)
+print("Multiplication is :",mult)
+print("Division is :",div)
